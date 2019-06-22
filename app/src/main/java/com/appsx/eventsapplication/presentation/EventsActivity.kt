bedf852d0777f1d3e1d3acdfb5ad494f68310909
DSemk <samik6677@gmail.com>
2019-06-22 14:05:18 +0300

    Updated path for EventsActivity

--- app/src/main/java/com/appsx/eventsapplication/presentation/EventsActivity.kt
+++ app/src/main/java/com/appsx/eventsapplication/presentation/EventsActivity.kt
@@ -1,7 +1,8 @@
-package com.appsx.eventsapplication
+package com.appsx.eventsapplication.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.appsx.eventsapplication.R
 
 class EventsActivity : AppCompatActivity() {
 
